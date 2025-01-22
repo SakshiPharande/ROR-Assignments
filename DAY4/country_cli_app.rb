cf816218e749f6bb2bcdd84ae5d0c33eb5e70c70
SakshiPharande <sakshi.pharande@joshsoftware.com>
2025-01-22 13:16:02 +0530

    add country-data in the form of constant

--- DAY4/country_cli_app.rb
+++ DAY4/country_cli_app.rb
@@ -1,15 +1,13 @@
-# frozen_string_literal: true
+# Module methods can still be used as helpers
+require './country_data' 
 
-require './country_data'
-
-# Corrected module name
 module CountryModule
-  def can_get_loan?(country)
+  def self.can_get_loan?(country)
     gdp_threshold = 8_845.63
     country[:development_status] == 'developing' && country[:gdp] < gdp_threshold
   end
 
-  def can_get_seat_in_un(country)
+  def self.can_get_seat_in_un(country)
     permanent_members = ['China', 'France', 'Russia', 'United Kingdom', 'United States']
     if permanent_members.include?(country[:name])
       puts "#{country[:name]} is a permanent member of the UN Security Council."
@@ -18,44 +16,33 @@ module CountryModule
     %w[developing developed].include?(country[:development_status])
   end
 
-  def can_win_war?(country)
+  def self.can_win_war?(country)
     country[:army_strength] > 223_000
   end
 end
 
-class CountryClass
-  include CountryModule
+# Function to analyze the country based on the constant data
+def analyze_country(country_name)
+  country = COUNTRIES_DATA[:countries].find { |c| c[:name] == country_name }
 
-  attr_reader :data
-
-  def initialize(data)
-    @data = data[:countries]
+  if country.nil?
+    puts "#{country_name} data not available."
+    return
   end
 
-  def analyze_country(country_name)
-    country = @data.find { |c| c[:name] == country_name }
+  loan_status = CountryModule.can_get_loan?(country) ? 'can get a loan.' : 'cannot get a loan.'
+  puts "#{country_name} #{loan_status}"
 
-    if country.nil?
-      puts "#{country_name} data not available."
-      return
-    end
+  un_seat_status = CountryModule.can_get_seat_in_un(country) ? "#{country_name} can have a seat at the UN." : "#{country_name} cannot have a seat at the UN."
+  puts un_seat_status
 
-    loan_status = can_get_loan?(country) ? 'can get a loan.' : 'cannot get a loan.'
-    puts "#{country_name} #{loan_status}"
-
-    un_seat_status = can_get_seat_un(country) ? "#{country_name} can have a seat at the UN." : "#{country_name} cannot have a seat at the UN."
-    puts un_seat_status
-
-    war_status = can_win_war?(country) ? "#{country_name} can win a war." : "#{country_name} cannot win a war."
-    puts war_status
-  end
+  war_status = CountryModule.can_win_war?(country) ? "#{country_name} can win a war." : "#{country_name} cannot win a war."
+  puts war_status
 end
 
-# Example of using the class and module
-countries_class = CountryClass.new(CountriesData.data)
-
+# Example of usage
 puts 'Enter the name of the country you want to analyze:'
 country_name = gets.chomp.strip.capitalize
 
 # Analyze the specified country
-countries_class.analyze_country(country_name)
+analyze_country(country_name)
--- DAY4/country_data.rb
+++ DAY4/country_data.rb
@@ -1,50 +1,47 @@
 # frozen_string_literal: true
 
-class CountriesData
-  def self.data
+# countries data as a constant
+COUNTRIES_DATA = {
+  countries: [
     {
-      countries: [
-        {
-          name: 'India',
-          population: 1_393_409_038,
-          gdp: 2_875.14, # in billion USD
-          states: ['Maharashtra', 'Uttar Pradesh', 'Bihar'],
-          army_strength: 1_390_000,
-          development_status: 'developing'
-        },
-        {
-          name: 'Germany',
-          population: 83_783_942,
-          gdp: 3_845.63, # in billion USD
-          states: %w[Bavaria Berlin Hesse],
-          army_strength: 184_000,
-          development_status: 'developed'
-        },
-        {
-          name: 'United States',
-          population: 331_002_651,
-          gdp: 21_137.00, # in billion USD
-          states: %w[California Texas Florida],
-          army_strength: 1_400_000,
-          development_status: 'developed'
-        },
-        {
-          name: 'Brazil',
-          population: 212_559_417,
-          gdp: 1_846.73, # in billion USD
-          states: ['São Paulo', 'Rio de Janeiro', 'Bahia'],
-          army_strength: 334_500,
-          development_status: 'developing'
-        },
-        {
-          name: 'Nigeria',
-          population: 206_139_589,
-          gdp: 448.12, # in billion USD
-          states: %w[Lagos Kano Rivers],
-          army_strength: 223_000,
-          development_status: 'developing'
-        }
-      ]
+      name: 'India',
+      population: 1_393_409_038,
+      gdp: 2_875.14, # in billion USD
+      states: ['Maharashtra', 'Uttar Pradesh', 'Bihar'],
+      army_strength: 1_390_000,
+      development_status: 'developing'
+    },
+    {
+      name: 'Germany',
+      population: 83_783_942,
+      gdp: 3_845.63, # in billion USD
+      states: %w[Bavaria Berlin Hesse],
+      army_strength: 184_000,
+      development_status: 'developed'
+    },
+    {
+      name: 'United States',
+      population: 331_002_651,
+      gdp: 21_137.00, # in billion USD
+      states: %w[California Texas Florida],
+      army_strength: 1_400_000,
+      development_status: 'developed'
+    },
+    {
+      name: 'Brazil',
+      population: 212_559_417,
+      gdp: 1_846.73, # in billion USD
+      states: ['São Paulo', 'Rio de Janeiro', 'Bahia'],
+      army_strength: 334_500,
+      development_status: 'developing'
+    },
+    {
+      name: 'Nigeria',
+      population: 206_139_589,
+      gdp: 448.12, # in billion USD
+      states: %w[Lagos Kano Rivers],
+      army_strength: 223_000,
+      development_status: 'developing'
     }
-  end
-end
+  ]
+}
